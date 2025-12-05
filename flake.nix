f03ca8f48a408e96e2a511b36972804284b1f621
Kilian Mayer <mayer-kilian@gmx.de>
2025-12-05 12:11:26 +0100

    edited flake to build on nixos

--- flake.nix
+++ flake.nix
@@ -1,12 +1,28 @@
 {
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = 
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "pomodoro-cli";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.alsa-lib ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           pkg-config
           alsa-lib
         ];
       };
-  };
+    };
 }
